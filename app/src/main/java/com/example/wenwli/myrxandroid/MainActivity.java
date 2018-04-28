b49a012253e3f0299e598a1ee6b01598dda756d1
FAREAST\v-wenwli <v-wenwli@microsoft.com>
2018-04-28 12:04:22 +0800

    Get the application package list
    1. Get the installed package beside system application
    2. Using RxJava to mapping and filter the applications, then upate UI

--- app/src/main/java/com/example/wenwli/myrxandroid/MainActivity.java
+++ app/src/main/java/com/example/wenwli/myrxandroid/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.wenwli.myrxandroid;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,9 +16,19 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import rx.Observable;
+import rx.Observer;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mAppRecyclerView;
+
+    private List<AppInfo> mAppInfos = new ArrayList<>();
     private AppAdapter mAdapter;
 
     @Override
@@ -24,22 +36,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<AppInfo> appInfos = new ArrayList<>();
-
-        AppInfo info = new AppInfo();
-        info.setAppIcon(null);
-        info.setAppName("QQ");
-
-        AppInfo info1 = new AppInfo();
-        info.setAppIcon(null);
-        info.setAppName("Wechat");
-
-        appInfos.add(info);
-        appInfos.add(info1);
+        mAdapter = new AppAdapter(mAppInfos);
 
         mAppRecyclerView = findViewById(R.id.app_list);
-        mAdapter = new AppAdapter(appInfos);
+        mAppRecyclerView.setHasFixedSize(true);
+        mAppRecyclerView.setLayoutManager(new LinearLayoutManager(this)); // TODO: Why can't create ViewHolder without set the LayoutManager
         mAppRecyclerView.setAdapter(mAdapter);
+
+        loadApp();
+    }
+
+    private void loadApp() {
+        final PackageManager pm = getPackageManager();
+
+        Observable.create(new Observable.OnSubscribe<ApplicationInfo>() {
+            @Override
+            public void call(Subscriber<? super ApplicationInfo> subscriber) {
+                List<ApplicationInfo> infos = pm.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
+                for (ApplicationInfo info : infos) {
+                    subscriber.onNext(info);
+                }
+                subscriber.onCompleted();
+            }
+        }).filter(new Func1<ApplicationInfo, Boolean>() {
+            @Override
+            public Boolean call(ApplicationInfo applicationInfo) {
+                return (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) <= 0;
+            }
+        }).map(new Func1<ApplicationInfo, AppInfo>() {
+            @Override
+            public AppInfo call(ApplicationInfo applicationInfo) {
+                AppInfo info = new AppInfo();
+                info.setAppName(applicationInfo.loadLabel(pm).toString());
+                info.setAppIcon(applicationInfo.loadIcon(pm));
+                return info;
+            }
+        }).subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Observer<AppInfo>() {
+            @Override
+            public void onCompleted() {
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(AppInfo appInfo) {
+                mAppInfos.add(appInfo);
+            }
+        });
     }
 
     private class AppHolder extends RecyclerView.ViewHolder {
@@ -70,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public AppHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.item_app_list, parent);
+            View view = getLayoutInflater().inflate(R.layout.item_app_list, parent, false);
             return new AppHolder(view);
         }
 
